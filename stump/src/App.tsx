00186398c231bae93623dcab89aeda2a822b4360
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-20 11:22:54 -0500

    TODO: implement newline func

--- stump/src/App.tsx
+++ stump/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className='codeBlockContainer'>
         <CodeBlock
-          content={"interface IProps {}"}
+          content={""}
           fileName={'test.tsx'}
         />
       </div>
